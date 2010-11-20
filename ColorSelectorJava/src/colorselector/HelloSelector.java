a7c77463a2fc44963088623a226463f61357d7d1
Rafael Ubiratam Clemente Afonso <rucafonso@hotmail.com>
2010-11-20 20:36:18 -0200

    Criação do projeto

---
+++ ColorSelectorJava/src/colorselector/HelloSelector.java
@@ -0,0 +1,12 @@
+package colorselector;
+
+public class HelloSelector {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HELLO COLOR SELECTOR!");
+	}
+
+}

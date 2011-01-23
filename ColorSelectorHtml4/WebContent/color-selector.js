179eec04b79e58d447484f475a22e187af4418da
rafonso <rucafonso@hotmail.com>
2011-01-22 22:13:33 -0200

    Correção na sincronização de valores. Geração de cores aleatórias.

--- ColorSelectorHtml4/WebContent/color-selector.js
+++ ColorSelectorHtml4/WebContent/color-selector.js
@@ -4,6 +4,11 @@ var COLOR_MAX = 255;
 
 var synchronizedSelects = [];
 
+/**
+ * Função que realiza a troca de cores do canvas.
+ */
+var changeColor;
+
 function $(id) {
 	return document.getElementById(id);
 }
@@ -22,17 +27,20 @@ function init() {
 	fillColorValues("blueValue");
 	fillColorValues("alphaValue");
 	
-	$("redValue").selectedIndex = $("redValue").selectedIndex = $("redValue").selectedIndex = 0;
+	$("redValue").selectedIndex = $("redValue").selectedIndex 
+		= $("redValue").selectedIndex = 0;
 	$("alphaValue").selectedIndex = COLOR_MAX;
-	$("selectRed").checked = $("selectGreen").checked = $("selectBlue").checked = $("selectAlpha").checked = $("enableAlpha").checked = false;
-	
+	$("selectRed").checked = $("selectGreen").checked = $("selectBlue").checked 
+		= $("selectAlpha").checked = $("enableAlpha").checked = false;
+	$("alphaValue").disabled = $("selectAlpha").disabled = true;
+
 	
 	changeColor = $("canvas").filters? 
-		function(r, g, b, a) {
+		function(r, g, b, a) { // IE
 			$("canvas").style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
 			$("canvas").style.filter = "alpha(opacity=" + (100 * a) + ")";
 		} : 
-		function(r, g, b, a) {
+		function(r, g, b, a) { // W3C
 			$("canvas").style.backgroundColor = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
 		};
 	
@@ -48,27 +56,36 @@ function enableAlpha(checkAlpha) {
 	}
 }
 
+function roundNumber(value) {
+	var result;
+	
+	if(value >= 100) {
+		result = value.toPrecision(3);
+	} else if(value >= 10) {
+		result = value.toPrecision(2);
+	} else {
+		result = value.toPrecision(1);
+	}
+	
+	return result;
+}
+
 function synchronizeValues(selectId, synchronize) {
 	var select = $(selectId);
+	select.synchronized =  synchronize;
 	var i = -1;
 	
-	if(synchronize) {
+	if(select.synchronized) {
 		synchronizedSelects.push(select);
 		if(synchronizedSelects.length > 1) {
 			// Calcula o valor médio.
-			select.synchronized = true;
 			var sum = 0;
 			for(i = 0; i < synchronizedSelects.length; i ++) {
 				sum += parseInt(synchronizedSelects[i].selectedIndex);
 			}
-			var media = sum / synchronizedSelects.length;
-			if(media >= 100) {
-				media = media.toPrecision(3);
-			} else if(media >= 10) {
-				media = media.toPrecision(2);
-			} else {
-				media = media.toPrecision(1);
-			}
+			
+			var media = Math.round(sum / synchronizedSelects.length);
+			
 			for(i = 0; i < synchronizedSelects.length; i ++) {
 				synchronizedSelects[i].selectedIndex = media;
 			}
@@ -77,17 +94,11 @@ function synchronizeValues(selectId, synchronize) {
 		for(i = 0; i < synchronizedSelects.length; i ++) {
 			if(synchronizedSelects[i].id == select.id) {
 				synchronizedSelects.splice(i, 1);
-				select.synchronized = false;
 				break;
 			}
 		}
 	}
 }
-
-/**
- * Função que realiza a troca de cores do canvas.
- */
-var changeColor;
 
 
 function valueChanged(selectSource) {
@@ -102,4 +113,22 @@ function valueChanged(selectSource) {
 	
 	var alpha = $("alphaValue").disabled? COLOR_MAX: ($("alphaValue").selectedIndex / COLOR_MAX);
 	changeColor($("redValue").selectedIndex, $("greenValue").selectedIndex, $("blueValue").selectedIndex, alpha);
+}
+
+function randomColor() {
+	var syncValue = Math.round(Math.random() * COLOR_MAX);
+
+	function setValue(id) {
+		var select = $(id);
+		select.selectedIndex = select.synchronized? syncValue: Math.round(Math.random() * COLOR_MAX);
+	}
+	
+	setValue("redValue");
+	setValue("greenValue");
+	setValue("blueValue");
+	if(!$("alphaValue").disabled) {
+		setValue("alphaValue");
+	}
+	
+	valueChanged(null);
 }

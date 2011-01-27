5dc8684b8e82a536cd5146ad2913a02078d8bc62
rafonso <rucafonso@hotmail.com>
2011-01-26 23:01:21 -0200

    Cores Web.

--- ColorSelectorHtml4/WebContent/color-selector.js
+++ ColorSelectorHtml4/WebContent/color-selector.js
@@ -1,4 +1,3 @@
-
 var COLOR_MIN = 0;
 var COLOR_MAX = 255;
 
@@ -14,39 +13,42 @@ function $(id) {
 }
 
 function init() {
-	
+
 	function fillColorValues(idSelect) {
 		var selectObject = $(idSelect);
-		for(var i = COLOR_MIN; i <= COLOR_MAX; i ++) {
+		for ( var i = COLOR_MIN; i <= COLOR_MAX; i++) {
 			selectObject.options[i] = new Option(i);
 		}
 	}
-	
+
 	fillColorValues("redValue");
 	fillColorValues("greenValue");
 	fillColorValues("blueValue");
 	fillColorValues("alphaValue");
-	
-	$("redValue").selectedIndex = $("redValue").selectedIndex 
-		= $("redValue").selectedIndex = 0;
+
+	$("redValue").selectedIndex = $("redValue").selectedIndex = $("redValue").selectedIndex = 0;
 	$("alphaValue").selectedIndex = COLOR_MAX;
-	$("selectRed").checked = $("selectGreen").checked = $("selectBlue").checked 
-		= $("selectAlpha").checked = $("enableAlpha").checked = false;
+	$("selectRed").checked = $("selectGreen").checked = $("selectBlue").checked = $("selectAlpha").checked = $("enableAlpha").checked = false;
 	$("alphaValue").disabled = $("selectAlpha").disabled = true;
 
-	
-	changeColor = $("canvas").filters? 
-		function(r, g, b, a) { // IE
-			$("canvas").style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
-			$("canvas").style.filter = "alpha(opacity=" + (100 * a) + ")";
-		} : 
-		function(r, g, b, a) { // W3C
-			$("canvas").style.backgroundColor = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
-		};
-	
+	changeColor = $("canvas").filters ? function(r, g, b, a) { // IE
+		$("canvas").style.backgroundColor = "rgb(" + r + ", " + g + ", " + b
+				+ ")";
+		$("canvas").style.filter = "alpha(opacity=" + (100 * a) + ")";
+	} : function(r, g, b, a) { // W3C
+		$("canvas").style.backgroundColor = "rgba(" + r + ", " + g + ", " + b
+				+ ", " + a + ")";
+	};
+
+	var selectWebColor = $("webColor");
+	for ( var i = 0; i < WEB_COLORS.length; i++) {
+		var webColor = WEB_COLORS[i];
+		selectWebColor.options[i] = new Option(webColor.name);
+	}
+
 	// Initialize Color Format Combo
 	var selectFormat = $("formatType");
-	for(var i = 0; i < formatters.length; i ++) {
+	for ( var i = 0; i < formatters.length; i++) {
 		selectFormat.options[i] = new Option(formatters[i].name);
 	}
 
@@ -56,51 +58,51 @@ function init() {
 function enableAlpha(checkAlpha) {
 	var enabled = checkAlpha.checked;
 	$("selectAlpha").disabled = $("alphaValue").disabled = !enabled;
-	if(!enabled) {
+	if (!enabled) {
 		$("selectAlpha").checked = false;
 		synchronizeValues("alphaValue", false);
 	}
-	
+
 	formatValue();
 }
 
 function roundNumber(value) {
 	var result;
-	
-	if(value >= 100) {
+
+	if (value >= 100) {
 		result = value.toPrecision(3);
-	} else if(value >= 10) {
+	} else if (value >= 10) {
 		result = value.toPrecision(2);
 	} else {
 		result = value.toPrecision(1);
 	}
-	
+
 	return result;
 }
 
 function synchronizeValues(selectId, synchronize) {
 	var select = $(selectId);
-	select.synchronized =  synchronize;
+	select.synchronized = synchronize;
 	var i = -1;
-	
-	if(select.synchronized) {
+
+	if (select.synchronized) {
 		synchronizedSelects.push(select);
-		if(synchronizedSelects.length > 1) {
+		if (synchronizedSelects.length > 1) {
 			// Calcula o valor médio.
 			var sum = 0;
-			for(i = 0; i < synchronizedSelects.length; i ++) {
+			for (i = 0; i < synchronizedSelects.length; i++) {
 				sum += parseInt(synchronizedSelects[i].selectedIndex);
 			}
-			
+
 			var media = Math.round(sum / synchronizedSelects.length);
-			
-			for(i = 0; i < synchronizedSelects.length; i ++) {
+
+			for (i = 0; i < synchronizedSelects.length; i++) {
 				synchronizedSelects[i].selectedIndex = media;
 			}
 		}
 	} else {
-		for(i = 0; i < synchronizedSelects.length; i ++) {
-			if(synchronizedSelects[i].id == select.id) {
+		for (i = 0; i < synchronizedSelects.length; i++) {
+			if (synchronizedSelects[i].id == select.id) {
 				synchronizedSelects.splice(i, 1);
 				break;
 			}
@@ -108,19 +110,33 @@ function synchronizeValues(selectId, synchronize) {
 	}
 }
 
-
 function valueChanged(selectSource) {
-//	console.log(selectSource.id + " - " + selectSource.value); 
-	if(selectSource && selectSource.synchronized && (synchronizedSelects.length > 1)) {
-		for(var i = 0; i < synchronizedSelects.length; i ++) {
-			if(synchronizedSelects[i].id != selectSource.id) {
+	// console.log(selectSource.id + " - " + selectSource.value);
+	if (selectSource && selectSource.synchronized
+			&& (synchronizedSelects.length > 1)) {
+		for ( var i = 0; i < synchronizedSelects.length; i++) {
+			if (synchronizedSelects[i].id != selectSource.id) {
 				synchronizedSelects[i].selectedIndex = selectSource.selectedIndex;
 			}
 		}
 	}
-	
-	var alpha = $("alphaValue").disabled? COLOR_MAX: ($("alphaValue").selectedIndex / COLOR_MAX);
-	changeColor($("redValue").selectedIndex, $("greenValue").selectedIndex, $("blueValue").selectedIndex, alpha);
+
+	var red = $("redValue").selectedIndex;
+	var green = $("greenValue").selectedIndex;
+	var blue = $("blueValue").selectedIndex;
+
+	var webColorIndex = 0;
+	for ( var i = 1; i < WEB_COLORS.length; i++) {
+		if (WEB_COLORS[i].isSameColor(red, green, blue)) {
+			webColorIndex = i;
+			break;
+		}
+	}
+	$("webColor").selectedIndex = webColorIndex; 
+
+	var alpha = $("alphaValue").disabled ? COLOR_MAX
+			: ($("alphaValue").selectedIndex / COLOR_MAX);
+	changeColor(red, green, blue, alpha);
 	formatValue();
 }
 
@@ -129,23 +145,36 @@ function randomColor() {
 
 	function setValue(id) {
 		var select = $(id);
-		select.selectedIndex = select.synchronized? syncValue: Math.round(Math.random() * COLOR_MAX);
+		select.selectedIndex = select.synchronized ? syncValue : Math
+				.round(Math.random() * COLOR_MAX);
 	}
-	
+
 	setValue("redValue");
 	setValue("greenValue");
 	setValue("blueValue");
-	if(!$("alphaValue").disabled) {
+	if (!$("alphaValue").disabled) {
 		setValue("alphaValue");
 	}
-	
+
 	valueChanged(null);
 }
 
 function formatValue() {
 	var formatter = formatters[$("formatType").selectedIndex];
-	
-	$("colorValue").value = formatter.format($("redValue").selectedIndex, 
-			$("greenValue").selectedIndex, $("blueValue").selectedIndex, 
+
+	$("colorValue").value = formatter.format($("redValue").selectedIndex,
+			$("greenValue").selectedIndex, $("blueValue").selectedIndex,
 			$("alphaValue").selectedIndex, $("enableAlpha").checked);
+}
+
+function changeWebColor() {
+	var selectedWebColor = WEB_COLORS[$("webColor").selectedIndex];
+
+	if (selectedWebColor.defined) {
+		$("redValue").selectedIndex = selectedWebColor.red;
+		$("greenValue").selectedIndex = selectedWebColor.green;
+		$("blueValue").selectedIndex = selectedWebColor.blue;
+
+		valueChanged();
+	}
 }

cca0be6989b25221a3d49b9b1473716b77367776
rafonso <rucafonso@hotmail.com>
2011-02-04 22:29:06 -0200

    Adição da formatação HSL. Formatação com alpha para RGB.

--- ColorSelectorJava/src/colorselector/Formatter.java
+++ ColorSelectorJava/src/colorselector/Formatter.java
@@ -28,20 +28,38 @@ public enum Formatter {
     },
     RGB("RGB") {
         private static final String RGB_FORMAT = "rgb(%3d, %3d, %3d)";
+        private static final String RGBA_FORMAT = "rgba(%3d, %3d, %3d, %.2f)";
+
+        /**
+         * See http://www.w3.org/TR/css3-color/#rgba-color
+         * 
+         * @param c
+         * @return
+         */
+        private String formatWithAlpha(Color c) {
+            return String.format(Locale.US, RGBA_FORMAT, c.getRed(),
+                    c.getGreen(), c.getBlue(), toHexProportion(c.getAlpha()));
+        }
+
+        /**
+         * See http://www.w3.org/TR/css3-color/#rgb-color
+         * 
+         * @param c
+         * @return
+         */
+        private String formatWithOutAlpha(Color c) {
+            return String.format(Locale.US, RGB_FORMAT, c.getRed(),
+                    c.getGreen(), c.getBlue());
+        }
 
         @Override
         public String format(Color c, boolean hasAlpha) {
-            return String.format(RGB_FORMAT, c.getRed(), c.getGreen(),
-                    c.getBlue());
+            return hasAlpha ? formatWithAlpha(c) : formatWithOutAlpha(c);
         }
     },
     PERCENT("Percent") {
         private final String RGB_FORMAT = "rgb(%3d%%, %3d%%, %3d%%)";
         private final String RGBA_FORMAT = "rgba(%3d%%, %3d%%, %3d%%, %.2f)";
-
-        private float toHexProportion(int value) {
-            return ((float) value) / 255;
-        }
 
         private int toHexPercent(int value) {
             return (int) (100 * toHexProportion(value));
@@ -75,12 +93,39 @@ public enum Formatter {
         public String format(Color c, boolean hasAlpha) {
             return hasAlpha ? formatWithAlpha(c) : formatWithOutAlpha(c);
         }
+    },
+    HSL("HSL") {
+
+        private final String HSL_FORMAT = "hsl(%.2f, %.2f, %.2f)";
+        private final String HSLA_FORMAT = "hsla(%.2f, %.2f, %.2f, %.2f)";
+
+        private String formatWithoutAlpha(float[] hsl) {
+            return String.format(Locale.US, HSL_FORMAT, hsl[0], hsl[1], hsl[2]);
+        }
+
+        private String formatWithAlpha(Color c, float[] hsl) {
+            return String.format(Locale.US, HSLA_FORMAT, hsl[0], hsl[1],
+                    hsl[2], super.toHexProportion(c.getAlpha()));
+        }
+
+        @Override
+        public String format(Color c, boolean hasAlpha) {
+            float[] hsl = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(),
+                    null);
+
+            return hasAlpha ? formatWithAlpha(c, hsl) : formatWithoutAlpha(hsl);
+        }
+
     };
 
     private String description;
 
     private Formatter(String description) {
         this.description = description;
+    }
+
+    protected float toHexProportion(int value) {
+        return ((float) value) / 255;
     }
 
     public abstract String format(Color c, boolean hasAlpha);
